bf66a849acdf975e6e25b3f479373964057f574f
moogun <moogunjung@gmail.com>
2018-10-19 13:03:37 +0900

    added validate fn to put method and added get method for /api/genres/:id

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/api/genres', (req, res) => {
     console.log('req', req.body);
     const { error } = validateGenre(req.body)
     if (error) return res.status(400).send(error.details[0].message)
-    
+
     const genre = {
          id: genres.length + 1,
          name: req.body.name
@@ -34,6 +34,9 @@ app.put('/api/genres/:id', (req, res) => {
     const genre = genres.find(c => c.id === parseInt(req.params.id))
     if (!genre) return res.status(404).send('The genre was not found')
 
+    const { error } = validateGenre(req.body)
+    if ( error ) return res.status(400).send(error.details[0].message)
+
     genre.name = req.body.name
     res.send(genre)
  })
@@ -45,6 +48,12 @@ app.delete('/api/genres/:id', (req, res) => {
     const index = genres.indexOf(genre)
     genres.splice(index, 1)
     res.send(genres)
+})
+
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id))
+    if(!genre) return res.status(404).send('The genre was not found')
+    res.send(genre)
 })
 
 const port = process.env.PORT || 3000;
