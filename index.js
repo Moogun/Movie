8da808baf527d7501ad7021315630bb84efa6722
moogun <moogunjung@gmail.com>
2018-10-19 13:49:37 +0900

    added mongoose and genreSchema

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const bodyParser = require('body-parser')
 const Joi = require('joi')
 const genres = require('./routes/genres')
 
+const mongoose = require('mongoose')
+
 app.use(bodyParser.json())
 genres(app)
+
+mongoose.connect('mongodb://localhost/movie')
+   .then(() => console.log('connected to MongoDB'))
+   .catch(err => console.log('Err', err))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
