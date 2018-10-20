1d3dc6f9a713ec35346362a71786c23d54544c63
moogun <moogunjung@gmail.com>
2018-10-20 10:17:04 +0900

    started working with db in genres router

--- routes/genres.js
+++ routes/genres.js
@@ -1,53 +1,45 @@
 const { Genre, validate } = require('../models/Genre')
 
-const genres = [
-    {id: 1, name: 'Action'},
-    {id: 2, name: 'Horror'},
-    {id: 3, name: 'Romance'}
-]
-
 module.exports = (app) => {
-    app.get('/api/genres', (req, res) => {
+    app.get('/api/genres', async (req, res) => {
+        const genres = await Genre.find({})
         res.send(genres)
     })
 
-    app.post('/api/genres', (req, res) => {
+    app.post('/api/genres', async (req, res) => {
         console.log('req', req.body);
         const { error } = validate(req.body)
         if (error) return res.status(400).send(error.details[0].message)
 
-        const genre = {
-             id: genres.length + 1,
-             name: req.body.name
-        }
-        genres.push(genre)
+        const genre = new Genre({
+            name: req.body.name
+        })
+        await genre.save()
         res.send(genre)
     })
 
-    app.put('/api/genres/:id', (req, res) => {
-        // let 0 = genres.filter(f => f.id == req.body.id)
-        //
-        const genre = genres.find(c => c.id === parseInt(req.params.id))
-        if (!genre) return res.status(404).send('The genre was not found')
-
+    app.put('/api/genres/:id', async (req, res) => {
         const { error } = validate(req.body)
         if ( error ) return res.status(400).send(error.details[0].message)
 
-        genre.name = req.body.name
+        const genre = await Genre.findByIdAndUpdate(req.params.id, {
+            name: req.body.name
+        }, {new: true})
+
+        if (!genre) return res.status(404).send('The genre was not found')
+
         res.send(genre)
      })
 
-    app.delete('/api/genres/:id', (req, res) => {
-        const genre = genres.find(c => c.id === parseInt(req.params.id))
+    app.delete('/api/genres/:id', async (req, res) => {
+        const genre = await Genre.findOneAndDelete(req.params.id)
         if (!genre) return res.status(404).send('The genre was not found')
 
-        const index = genres.indexOf(genre)
-        genres.splice(index, 1)
-        res.send(genres)
+        res.send(genre)
     })
 
-    app.get('/api/genres/:id', (req, res) => {
-        const genre = genres.find(c => c.id === parseInt(req.params.id))
+    app.get('/api/genres/:id', async (req, res) => {
+        const genre = await Genre.findById(req.params.id)
         if(!genre) return res.status(404).send('The genre was not found')
         res.send(genre)
     })
