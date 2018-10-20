866a916c81edde66c7e26b5ab31f9db367785ffc
moogun <moogunjung@gmail.com>
2018-10-20 10:05:53 +0900

    added custoemr put, delete methods and etc

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,9 @@
 const { Customer, validate } = require('../models/Customer')
 
 module.exports = (app) => {
-    app.get('/api/customers', (req, res) => {
-        res.send('customers')
+    app.get('/api/customers', async (req, res) => {
+        const customers = await Customer.find({})
+        res.send(customers)
     })
 
     app.post('/api/customers', async (req, res) => {
@@ -19,4 +20,29 @@ module.exports = (app) => {
         res.send(customer)
     })
 
+    app.put('/api/customers/:id', async (req, res) => {
+
+        const { error } = validate(req.body)
+        if (error) return res.status(400).send(error.details[0].message)
+
+        const customer = await Customer.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            isGold: req.body.isGold,
+            phone: req.body.phone
+        }, {new: true})
+
+        if (!customer) return res.status(404).send('The customer was not found')
+        res.send(customer)
+    })
+
+    app.delete('/api/customers/:id', async (req, res) => {
+        const customer = await Customer.findByIdAndRemove(req.params.id)
+        if(!customer) return res.status(404).send('The customer was not found')
+        res.send(customer)
+    })
+
+    app.get('/api/customers/:id', async (req, res) => {
+        const customer = await Customer.findById(req.params.id)
+        res.send(customer)
+    })
 }
