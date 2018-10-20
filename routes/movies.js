50a059ec228808af45821b46508b3e94e3402a1c
moogun <moogunjung@gmail.com>
2018-10-20 11:27:22 +0900

    added post method for /api/movies

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router()
 const { Movie, validate } = require('../models/Movie')
+const { Genre } = require('../models/Genre')
 
 router.get('/', async (req, res) => {
     const movie = await Movie.find({})
+    res.send(movie)
+})
+
+router.post('/', async (req, res) => {
+    console.log('req', req.body);
+    const { error } = validate(req.body)
+    if (error) res.status(404).send(error.details[0].message)
+
+    const genre = await Genre.findById(req.body.genreId)
+    if (!genre) return res.status(400).send('Invaid genre')
+
+    const movie = new Movie({
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    })
+    await movie.save()
     res.send(movie)
 })
 
