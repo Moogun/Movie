ca0128bf61421342bf6fef427daf452a9a11c7ef
moogun <moogunjung@gmail.com>
2018-10-20 11:31:15 +0900

    added put method for /api/movies/:id

--- routes/movies.js
+++ routes/movies.js
@@ -29,4 +29,25 @@ router.post('/', async (req, res) => {
     res.send(movie)
 })
 
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body)
+    if (error) res.status(404).send(error.details[0].message)
+
+    const genre = await Genre.findById(req.body.genreId)
+    if (!genre) return res.status(400).send('Invaid genre')
+
+    const movie = await Movie.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    }, {new: true})
+
+    if (!movie) res.status(404).send('No movie found')
+    res.send(movie)
+})
+
 module.exports = router
