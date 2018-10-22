3ca55001f010800bf70104926732635ddc8f8254
moogun <moogunjung@gmail.com>
2018-10-22 11:34:35 +0900

    added GET /:id test for genres

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,15 +1,43 @@
 const request = require('supertest');
-
+const { Genre } = require('../../models/Genre');
 let server;
 
 describe('/api/genres', () => {
 
     beforeEach(() => { server = require('../../index')})
-    afterEach(() => { server.close() })
+    afterEach(async () => {
+        server.close()
+        await Genre.remove({})
+    })
+
     describe('GET /', () => {
         it('should return all genres', async () => {
+            await Genre.collection.insertMany([
+                {name: 'genre1'},
+                {name: 'genre2'}
+            ])
+
             const res = await request(server).get('/api/genres')
             expect(res.status).toBe(200)
+            expect(res.body.length).toBe(2)
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy()
+            expect(res.body.some(g => g.name === 'genre2')).toBeTruthy()
+        })
+    })
+
+    describe('GET /:id', () => {
+        it('should return a genre if valid id is passed', async () => {
+            const genre = new Genre({name: 'genre1'})
+            await genre.save()
+
+            const res = await request(server).get('/api/genres/' + genre._id )
+            console.log('--slakjg', typeof res.body._id, typeof 'abc' === 'string');
+            expect(res.status).toBe(200)
+            expect(res.body).toHaveProperty('name', genre.name)
+            // expect(res.body._id).toMatchObject(genre._id)
+            // res.body._id is string
+            // this will fail cause genre._id prop is an obj
+
         })
     })
 })
