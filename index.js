19ee592748b0317d9cf37b6081c30cc2c37eade9
moogun <moogunjung@gmail.com>
2018-10-22 18:42:45 +0900

    updated index for a moment

--- index.js
+++ index.js
@@ -25,9 +25,12 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 3000;
 // app.listen(port, () => winston.info(`Listening on port ${port}...`));
-const server = app.listen(port, () => winston.add(
+// const server = app.listen(port, () => winston.add(
+    // new winston.transports.Console((`Listening on port ${port}...`))
+// ));
+app.listen(port, () => winston.add(
     new winston.transports.Console((`Listening on port ${port}...`))
 ));
 
 
-module.exports = server
+// module.exports = server
