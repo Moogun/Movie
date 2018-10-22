91911a34cdd8057218ab1f2e3e9139bf71908c20
moogun <moogunjung@gmail.com>
2018-10-22 16:18:51 +0900

    added a unit test for auth middleware req obj

--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -1,6 +1,31 @@
+// Unit test for req.user part 
+/*
+try {
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'))
+    req.user = decoded
+    next()
+}
+*/
+
+const { User } = require('../../../models/User')
+const auth = require('../../../middlewares/auth')
+const mongoose = require('mongoose')
 
 describe('auth middleware', () => {
     it('should populate req.user with the payload of a valid JWT', () => {
-        // const token = new User().generateAuthToken()
+        const user = {
+            _id: mongoose.Types.ObjectId().toHexString(),
+            isAdmin: true
+        }
+        const token = new User(user).generateAuthToken()
+        const req = {
+            header: jest.fn().mockReturnValue(token)
+        }
+        const res = {}
+        const next = jest.fn()
+
+        auth(req, res, next)
+
+        expect(req.user).toMatchObject(user)
     })
 })
