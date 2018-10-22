1607029b4464c62c262bfbcd0a1c0bde5d4ea82d
moogun <moogunjung@gmail.com>
2018-10-22 16:11:12 +0900

    added integration auth test

---
+++ tests/unit/middleware/auth.test.js
@@ -0,0 +1,6 @@
+
+describe('auth middleware', () => {
+    it('should populate req.user with the payload of a valid JWT', () => {
+        // const token = new User().generateAuthToken()
+    })
+})
